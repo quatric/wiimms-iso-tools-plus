08bdce5649bb76f02593268c339a42f45011b980
quatric <quatric@users.noreply.github.com>
2026-09-10 18:31:11 -0500

    archives: split Next Level Games Texture To Go (.txtg / 6PK0) into lib-txtg

---
+++ project/src/lib-txtg.h
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: GPL-2.0+
+#ifndef LIB_TXTG_H
+#define LIB_TXTG_H 1
+
+#include "lib-nintendo.h"
+
+// Next Level Games Texture To Go (.txtg / 6PK0)
+enumError ExtractTXTGArchive (ccp arg, ccp basedir, uint depth);
+
+#endif // LIB_TXTG_H
