3256526018cf4e31949e1ad5f51789f7bb790b82
quatric <quatric@users.noreply.github.com>
2026-09-10 18:34:19 -0500

    archives: split Koei Tecmo G1T texture container into lib-g1t

---
+++ project/src/lib-g1t.h
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: GPL-2.0+
+#ifndef LIB_G1T_H
+#define LIB_G1T_H 1
+
+#include "lib-nintendo.h"
+
+// Koei Tecmo G1T texture container
+enumError ExtractG1TArchive (ccp arg, ccp basedir, uint depth);
+
+#endif // LIB_G1T_H
