027af12f1d4c8619922d517ffda8d74011bb7783
quatric <quatric@users.noreply.github.com>
2026-09-10 18:29:22 -0500

    archives: split Nintendo Binary Audio Resource Archive (.bars / BARS) into lib-bars

---
+++ project/src/lib-bars.h
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: GPL-2.0+
+#ifndef LIB_BARS_H
+#define LIB_BARS_H 1
+
+#include "lib-nintendo.h"
+
+// Nintendo Binary Audio Resource Archive (.bars / BARS)
+enumError ExtractBARSArchive (ccp arg, ccp basedir, uint depth);
+
+#endif // LIB_BARS_H
