c6cbd556f540b4ed27d2cc8ff1023321bf2b21d5
quatric <quatric@users.noreply.github.com>
2026-09-10 18:32:39 -0500

    archives: split Koei Tecmo / Gust Texture Volume Archive (.tvol) into lib-tvol

--- project/src/lib-nintendo-archives.h
+++ project/src/lib-nintendo-archives.h
@@ -4,6 +4,7 @@
 
 #include "types.h"
 #include "lib-nintendo.h"
+#include "lib-tvol.h"
 #include "lib-xtx.h"
 #include "lib-romfs.h"
 #include "lib-txtg.h"
---
+++ project/src/lib-tvol.h
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: GPL-2.0+
+#ifndef LIB_TVOL_H
+#define LIB_TVOL_H 1
+
+#include "lib-nintendo.h"
+
+// Koei Tecmo / Gust Texture Volume Archive (.tvol)
+enumError ExtractTVOLArchive (ccp arg, ccp basedir, uint depth);
+
+#endif // LIB_TVOL_H
