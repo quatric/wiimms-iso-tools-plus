23c8e53e5778c99d73bf56b3564c534a0213b106
quatric <quatric@users.noreply.github.com>
2026-09-10 18:35:45 -0500

    archives: split Camelot GX texture bank (Mario Golf / Mario Power Tennis) into lib-camtexbank

---
+++ project/src/lib-camtexbank.h
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: GPL-2.0+
+#ifndef LIB_CAMTEXBANK_H
+#define LIB_CAMTEXBANK_H 1
+
+#include "lib-nintendo.h"
+
+// Camelot GX texture bank (Mario Golf / Mario Power Tennis)
+enumError ExtractCamelotTexBank (ccp arg, ccp basedir, uint depth);
+
+#endif // LIB_CAMTEXBANK_H
--- project/src/lib-nintendo-archives.h
+++ project/src/lib-nintendo-archives.h
@@ -4,6 +4,7 @@
 
 #include "types.h"
 #include "lib-nintendo.h"
+#include "lib-camtexbank.h"
 #include "lib-pers.h"
 #include "lib-zdat.h"
 #include "lib-g1t.h"
